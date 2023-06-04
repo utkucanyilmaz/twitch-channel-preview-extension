42239a28268550d53c02031be600ec1d65edabf0
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-06-04 18:27:03 +0300

    Refactored and deleted unnecessary code

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 let imageUrl = "";
 
 const observer = new MutationObserver(mutation => {
-  mutationCallbackFn(mutation);
   updateEventListeners();
+  mutationCallbackFn(mutation);
 });
 
 observer.observe(document.body, {
@@ -12,7 +12,7 @@ observer.observe(document.body, {
 document.addEventListener("DOMContentLoaded", () => updateEventListeners());
 
 function updateImageUrl(user) {
-  imageUrl = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${user}-426x240.jpg?dummy=${new Date().getTime()}`;
+  imageUrl = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${user}-190x107.jpg`;
 }
 
 function checkExistingImage() {
@@ -27,49 +27,50 @@ function checkExistingImage() {
 }
 
 function updateEventListeners() {
-  const sideNav = document.getElementsByClassName("jKBAWW");
+  let sideNav;
+  if (window.innerWidth < 1200) {
+    sideNav = document.querySelectorAll(".side-nav-card.tw-link");
+  } else {
+    sideNav = document.querySelectorAll(".side-nav-card__link.tw-link");
+  }
   for (let i = 0; i < sideNav.length; i++) {
     sideNav[i].addEventListener("mouseenter", event => {
-      if (event.currentTarget.classList.contains("side-nav-card__link")) {
-        const user = event.currentTarget.attributes.href.value.slice(1);
-        updateImageUrl(user);
-        checkExistingImage();
-      }
+      const user = event.currentTarget.attributes.href.value.slice(1);
+      updateImageUrl(user);
+      checkExistingImage();
     });
   }
 }
 
 function mutationCallbackFn(mutationRecords) {
+  let nodes;
+  let nodesArr;
   mutationRecords.forEach(mutation => {
-    const nodes = mutation.target.querySelectorAll("*");
-    let deepestNode = nodes[nodes.length - 1];
-    while (deepestNode.children.length > 0) {
-      deepestNode = deepestNode.children[0];
-    }
-
-    if (deepestNode.classList.contains("UHEOX")) {
-      const parentElement = deepestNode.parentNode;
-      const existingImage = parentElement.querySelector("#sidenav-image");
-      if (existingImage) {
-        existingImage.onload = function () {
-          existingImage.onload = null;
-        };
-        existingImage.src = "";
-        existingImage.src = imageUrl;
-      } else {
-        const newElement = document.createElement("img");
-        newElement.id = "sidenav-image";
-        newElement.onload = function () {
-          newElement.onload = null;
-        };
-        newElement.src = "";
-        newElement.src = imageUrl;
-        parentElement.appendChild(newElement);
-      }
-    }
-
-    deepestNode.addEventListener("DOMCharacterDataModified", () => {
-      checkExistingImage();
-    });
+    nodes = mutation.target.querySelectorAll("*");
+    nodesArr = Array.from(nodes);
   });
+
+  const targetNode = nodesArr.filter(node => node.classList.contains("dIzyTl"));
+
+  if (targetNode.length > 0) {
+    const parentElement = targetNode[0];
+    const existingImage = parentElement.querySelector("#sidenav-image");
+    if (existingImage) {
+      existingImage.onload = function () {
+        existingImage.onload = null;
+      };
+      existingImage.src = "";
+      existingImage.src = imageUrl;
+    } else {
+      const newElement = document.createElement("img");
+      newElement.id = "sidenav-image";
+      newElement.onload = function () {
+        newElement.onload = null;
+      };
+      newElement.src = "";
+      newElement.src = imageUrl;
+      newElement.style.padding = "5px";
+      parentElement.prepend(newElement);
+    }
+  }
 }
