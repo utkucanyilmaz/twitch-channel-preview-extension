391ea5af88a03178832598dfd0bc1bbc84ec8646
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-06-06 16:17:24 +0300

    Fixed the issue of assigning multiple event listeners to same element

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
 let imageUrl = "";
 
 const observer = new MutationObserver(mutation => {
-  updateEventListeners();
   mutationCallbackFn(mutation);
 });
 
@@ -9,37 +8,38 @@ observer.observe(document.body, {
   childList: true,
 });
 
-document.addEventListener("DOMContentLoaded", () => updateEventListeners());
-
 function updateImageUrl(user) {
   imageUrl = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${user}-190x107.jpg`;
 }
 
 function checkExistingImage() {
   const existingImage = document.getElementById("sidenav-image");
+
   if (existingImage) {
+    existingImage.src = "";
     existingImage.onload = function () {
       existingImage.onload = null;
     };
-    existingImage.src = "";
     existingImage.src = imageUrl;
   }
 }
 
 function updateEventListeners() {
-  let sideNav;
-  if (window.innerWidth < 1200) {
-    sideNav = document.querySelectorAll(".side-nav-card.tw-link");
-  } else {
-    sideNav = document.querySelectorAll(".side-nav-card__link.tw-link");
-  }
+  const sideNav =
+    window.innerWidth < 1200
+      ? document.querySelectorAll(".side-nav-card.tw-link")
+      : document.querySelectorAll(".side-nav-card__link.tw-link");
+
   for (let i = 0; i < sideNav.length; i++) {
-    sideNav[i].addEventListener("mouseenter", event => {
-      const user = event.currentTarget.attributes.href.value.slice(1);
-      updateImageUrl(user);
-      checkExistingImage();
-    });
+    sideNav[i].removeEventListener("mouseenter", mouseEnterHandler);
+    sideNav[i].addEventListener("mouseenter", mouseEnterHandler);
   }
+}
+
+function mouseEnterHandler(event) {
+  const user = event.currentTarget.attributes.href.value.slice(1);
+  updateImageUrl(user);
+  checkExistingImage();
 }
 
 function mutationCallbackFn(mutationRecords) {
@@ -54,27 +54,18 @@ function mutationCallbackFn(mutationRecords) {
       const targetNode = nodesArr.filter(node =>
         node.classList.contains("dIzyTl")
       );
-      if (targetNode.length > 0) {
-        const parentElement = targetNode[0];
-        const existingImage = parentElement.querySelector("#sidenav-image");
-        if (existingImage) {
-          existingImage.onload = function () {
-            existingImage.onload = null;
-          };
-          existingImage.src = "";
-          existingImage.src = imageUrl;
-        } else {
-          const newElement = document.createElement("img");
-          newElement.id = "sidenav-image";
-          newElement.onload = function () {
-            newElement.onload = null;
-          };
-          newElement.src = "";
-          newElement.src = imageUrl;
-          newElement.style.padding = "5px";
-          parentElement.prepend(newElement);
-        }
-      }
+      updateEventListeners();
+      const parentElement = targetNode[0];
+
+      const newElement = document.createElement("img");
+      newElement.id = "sidenav-image";
+      newElement.src = "";
+      newElement.onload = function () {
+        newElement.onload = null;
+      };
+      newElement.src = imageUrl;
+      newElement.style.padding = "5px";
+      parentElement.prepend(newElement);
     }
     return;
   });
